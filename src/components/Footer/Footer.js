2655c398921b310a18557f1be7537b4957332325
CPrich905 <charlie.prichard@gmail.com>
2019-11-29 17:57:44 +0000

    footer styled & clicks

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -8,11 +8,19 @@ class Footer extends React.Component {
   }
 
   hideFooter() {
-    const footerNote = document.getElementById('footer-note')
-    if (footerNote.style.display === 'none') {
-      footerNote.style.display = 'block'
-    } else {
-      footerNote.style.display = 'none'
+    // const footerDiv = document.querySelector('footer')
+    const footerNotes = document.querySelectorAll('.footer-text')
+    const authorsNote = document.getElementById('authors-note')
+    let i = 0
+    // console.log('footerNote', footerNotes[1])
+    for (i = 0; i < footerNotes.length; i++) {
+      if (footerNotes[i].style.display === 'none') {
+        footerNotes[i].style.display = 'block'
+        authorsNote.style.display = 'none'
+      } else {
+        footerNotes[i].style.display = 'none'
+        authorsNote.style.display = 'block'
+      }
     }
   }
 
@@ -21,8 +29,7 @@ class Footer extends React.Component {
       <div className="footer" onClick={this.hideFooter}>
         <h4 className="footer-banner">Page & Story by Me. <a href="https://www.linkedin.com/in/charles-prichard/">LinkedIn</a></h4>
         <div className="footer-note" id="footer-note">
-          <p className="footer-text">Authors note: This is quite a big project to write and develop so some of the chapters are yet to be fully written. There are bullet point notes for the incomplete chapters which will hopefully give you an idea of the story arc I have in mind - if you have any questions or suggestions, please get in touch through LinkedIn.</p>
-          <p>Click anywhere in the footer to hide this message, click again to bring it back.</p>
+          <p><span id="authors-note" className="authors-note">Click to show authors note </span><span className="footer-text">This is quite a big project to write and develop so some of the chapters are yet to be fully written. There are bullet point notes for the incomplete chapters which will hopefully give you an idea of the story arc I have in mind - if you have any questions or suggestions, please get in touch through LinkedIn.</span></p>
         </div>
       </div>
     )
