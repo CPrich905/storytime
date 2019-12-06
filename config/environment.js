5ceafcbb4025f495f08dd7a4ccb1ba5d81d79227
CPrich905 <charlie.prichard@gmail.com>
2019-12-06 14:50:11 +0000

    debugging deployment - dbURI

--- config/environment.js
+++ config/environment.js
@@ -1,4 +1,4 @@
 const port = process.env.PORT || 4000
-const dbURI = process.env.MONGODB_URI || 'mongodb://localhost/storytime'
+const dbURI = process.env.MONGODB || 'mongodb://localhost/storytime'
 
 module.exports = { port, dbURI }
