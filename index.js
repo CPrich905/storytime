af467cd16d2c3745b186d8650dd5c27e92b78fd7
CPrich905 <charlie.prichard@gmail.com>
2019-12-06 13:34:07 +0000

    errorHandler & logger.js built & imported

--- index.js
+++ index.js
@@ -2,18 +2,24 @@ const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const app = express()
-
+const logger = require('./lib/logger')
+const errorHandler = require('./lib/errorHandler')
 const router = require('./config/router')
+
 const { port, dbURI } = require('./config/environment')
 
-mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
-
-app.use(bodyParser.json())
+mongoose.connect(dbURI, { useNewUrlParser: true, useCreateIndex: true })
 
 app.use(express.static(`${__dirname}/dist`))
+
+app.use(bodyParser.json())
 
 app.use('/api', router)
 
 app.get('/*', (req, res) => res.sendFile(`${__dirname}/dist/index.html`))
+
+app.use(errorHandler)
+
+app.use(logger)
 
 app.listen(port, () => console.log(`App is listening on port ${port}`))
